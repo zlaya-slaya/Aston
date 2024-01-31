1e5708234535d7b0937b37754bbce53d071b802b
zlaya_slaya <slaevaai@gmail.com>
2024-01-31 18:50:23 +0400

    zadanie_7
    Signed-off-by: zlaya_slaya <slaevaai@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,43 @@
 package org.example;
+import java.util.Arrays;
 
-public class Main{
+
+public class Main {
     public static void main(String[] args) {
-       //Задание №1
+        String[][] array = {{"546", "664", "3", "47"},
+                            {"52", "56", "743", "998"},
+                            {"766", "164", "114", "12"},
+                            {"135", "14", "15", "1226"}};
 
-        Cat cat1 = new Cat("Том");
-        cat1.running(20);
-        cat1.swimming(10);
-
-        Dog dog1 = new Dog("Пёсель");
-        dog1.running(499);
-        dog1.swimming(11);
-
-
-        Bowl bowl1 = new Bowl(55);
-
-        Cat[] arrayCat = {new Cat("Первый"),
-                new Cat("Второй"),
-                new Cat("Третий"),
-                new Cat("Четверный"),
-                new Cat("Пятый"),
-                new Cat("Шестой")};
-        for (int i = 0; i < arrayCat.length; i++ ){
-            arrayCat[i].eatFood(bowl1, 11);
+        try {
+            System.out.println(calculateSumArray(array));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
         }
-        for (int i = 0; i < arrayCat.length; i++){
-            arrayCat[i].getWellFed();
+    }
+
+
+    public static int calculateSumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int row = 4;
+        int column = 4;
+        if (array.length != row || array[0].length != column)
+            throw new MyArraySizeException("Ошибка в размере массива: ожидаемый размер 4х4");
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверный формат данных в ячейке: [" + i + "][" + j + "]");
+                }
+            }
         }
-
-
-        bowl1.addFood(30);
-        bowl1.getFood();
-
-
-        Cat.getCount();
-        Dog.getCount();
-        Animal.getCount();
-
-
-    //Задание №2
-
-        Rectangle r1 = new Rectangle(4, 5, "Черный", "Белый");
-        r1.getAllInfo();
-
-        Triangle t1 = new Triangle(3, 4, 5, "Розовый", "Салатовый");
-        t1.getAllInfo();
-
-        Circle c1 = new Circle(5, "Голубой", "Синий");
-        c1.getAllInfo();
+        return sum;
     }
 }
+
+
+
+
 
---
+++ src/main/java/org/example/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
+
